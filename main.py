dd0239418452729a93e19e0afbbef2fc37f88965
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-25 03:09:58 +0000

    Get the list of songs

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ print('Which music year do you want to create a playlist from?')
 
 # Get the date
 date = input('Type in this format YYYY-MM-DD: ')
-print(date)
+
+# Get the list of music from that date from Billboard Hot 100
+BILLBOARD_URL = "https://www.billboard.com/charts/hot-100"
+BILLBOARD_ENDPOINT = f"{BILLBOARD_URL}/{date}"
+
+response = requests.get(BILLBOARD_ENDPOINT)
+response.raise_for_status()
+data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+song_list = soup.find_all(name="span", class_="chart-element__information__song")
+songs = [song.getText() for song in song_list]
+print(songs)
